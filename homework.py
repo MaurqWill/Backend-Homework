6956ac91464a383fa76c988260131837179cdd63
MaurqWill <mwilliams.mw74@gmail.com>
2024-03-19 10:04:30 -0400

    Update

--- homework.py
+++ homework.py
@@ -1,1 +1,76 @@
 print("Lets get it")
+# Code Correction
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Your Mood Today
+mood = "happy"
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
+
+# # Python Naming Convention Practice
+# pi_value = class = "hljs_number" > 3.14
+# userage = class = "hljs_number" > 25
+# user_location = class = "hljs_string" > "New York"
+# maxlimit = class = "hljs_number" > 1000
+
+# Python Data Types and type() Function
+variable_a = "Hello, World!"
+print(type(variable_a))
+# variable_a = <class 'str'>
+
+variable_b = 23
+print(type(variable_b))
+# variable_b = <class 'str'>
+
+variable_c = 3.14
+print(type(variable_c))
+#variable_c = <class 'str'>
+
+variable_d = True
+print(type(variable_d))
+#variable_d = <class 'bool'>
+
+# Grocery Store Math
+grocery_list = input("What would you like?")
+#print(user_input)
+bread = "3"
+peanut_butter = "2"
+jelly = "2"
+pbj = "bread", "peanut_butter", "jelly"
+bread_price = int(bread)
+peanut_butter_price = int(peanut_butter)
+jelly_price = int(jelly)
+pbj_price = bread_price + peanut_butter_price + jelly_price
+print(pbj_price)
+
+# Value Swapping
+x = 2
+y = 3
+x = 3
+y = 2
+x == y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
